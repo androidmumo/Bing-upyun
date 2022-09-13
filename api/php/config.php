94896f52feb304a967ad4ee2eb29368c8dc5080d
ZhangXQA <z1304242002@163.com>
2022-09-13 16:53:14 +0800

    Update config.php

--- api/php/config.php
+++ api/php/config.php
@@ -8,16 +8,16 @@ Copyright © 2020 by m@mcloc.cn
 $config['startdate']     = '****-**-**';
 
 //又拍云连接信息
-$config['bucketName']    = 'bing';  //你的又拍云存储库
-$config['operatorName']  = 'bing';  //你的存储库操作员
-$config['operatorPwd']   = '';  //你的存储库操作员密码
-$config['domainName']    = '';  //又拍云加速域名。注：结尾的 / 不能省略。如：'https://upyun.yourdom.com/'
+$config['bucketName']    = '********';  //你的又拍云存储库
+$config['operatorName']  = '********';  //你的存储库操作员
+$config['operatorPwd']   = '********';  //你的存储库操作员密码
+$config['domainName']    = '********';  //又拍云加速域名。注：结尾的 / 不能省略。如：'https://upyun.yourdom.com/'
 
 //数据库信息
-$config['mysqlHost']     = 'localhost:3306';  //MySQL数据库主机名
-$config['mysqlUsername'] = 'bing';  //MySQL数据库用户名
-$config['mysqlPassword'] = '';  //MySQL数据库密码
-$config['mysqlDbname']   = '';  //MySQL数据库名
+$config['mysqlHost']     = '********';  //MySQL数据库主机名
+$config['mysqlUsername'] = '********';  //MySQL数据库用户名
+$config['mysqlPassword'] = '********';  //MySQL数据库密码
+$config['mysqlDbname']   = '********';  //MySQL数据库名
 
 //延时
 $config['delay'] = 90; //默认延时90s，不建议修改
